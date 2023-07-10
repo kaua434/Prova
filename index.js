d896ac841cc3d241ba059cc055ef1bb4efb5eb69
Kauã dos Santos <109426409+kaua434@users.noreply.github.com>
2023-07-10 12:34:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+//atv 1
+
+// const http = require('http')
+// const server = http.createServer ((req, res) => {
+
+//     res.setHeader('Content-Type', 'applicaion/json');
+//     res.writeHead(200('Content-Type', 'text/plain'))
+//     res.end()
+// })
+
+// const objeto = [
+//     {id: 1, nome: 'Carlos'},
+//     {id: 2, nome: 'Bruno'},
+//     {id: 3, nome: 'Leticia'},
+// ]
+
+// const json = JSON.stringify(objeto)
+// console.log(json)
+
+
+//atv 2
+
+
+//atv 3
+//  const url = require('url')
+//  const myURL = new URL('https://nodejs.org/api/url.html#url_new_url_input_base'); 
+
+//  Url {
+//     protocol: 'https',
+//     slashes: true,
+//     auth: null,
+//     host: 'nodejs.org',
+//     port: null,
+//     hostname: 'nodejs.org',
+//     hash:'#url_new_url_input_base',
+//     search: null,
+//     query: [Object: null prototype] {},
+//     pathname: '/api/url.html',
+//     path: '/api/url.html',
+//     href: 'https://nodejs.org/api/url.html#url_new_url_input_base'
+//  }
+
+
+//atv 4
+
+
+//atv 5
+
+// const os = require('os'); 
+// [
+//     {
+//       model: '11th Gen Intel(R) Core(TM) i5-1135G& @ 2.40GHz',
+//       speed: 2419,
+//       times: {
+//         user: 146031,
+//         nice: 0,
+//         sys: 96859,
+//         idle: 63269921,
+//         irq: 5031,
+//       },
+//     },
+//   ] 
+// console.log(os.cpus())
